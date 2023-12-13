a452d091c9834a55dd1c0bf790bdbf128dc95734
Corneel Eijsbouts <Corneel.Eijsbouts@ordina.nl>
2023-12-14 00:45:47 +0100

    Day 13, part 2

--- 2023/aoc13.js
+++ 2023/aoc13.js
@@ -9,18 +9,36 @@ const checkMirrorOnLine = (pattern, line) => {
     const size = pattern.length
 
     let match = true
+    let smudges = 0
 
     let offset = 0
     while (match && offset < line && offset + line < size) {
         match = pattern[line - 1 - offset] === pattern[line + offset]
+
+        if (!match) {
+            const line1 = pattern[line - 1 - offset].split('')
+            const line2 = pattern[line + offset].split('')
+
+            let misses = 0
+            line1.forEach((line1Rock, index) => {
+                if (line1Rock !== line2[index]) misses++
+            })
+
+            if (misses === 1) {
+                smudges++
+                match = true
+            }
+        }
         offset++
     }
 
-    return match
+    return [match, smudges]
 }
-const findHorizontalMirror = (pattern) => {
+
+const findHorizontalMirror = (pattern, amountOfSmudges) => {
     for (let i = 1; i < pattern.length; i++) {
-        if (checkMirrorOnLine(pattern, i)) return i
+        const [mirror, smudges] = checkMirrorOnLine(pattern, i)
+        if (mirror && smudges === amountOfSmudges) return i
     }
     return 0
 }
@@ -43,11 +61,13 @@ const turnPattern = (pattern) => {
     return turnedPattern
 }
 
+for (let smudges = 0; smudges <= 1; smudges++) {
 
-const mirrors = patterns.slice(0).map(pattern => {
-    let mirror = findHorizontalMirror(pattern) * 100
-    if (mirror === 0) mirror = findHorizontalMirror(turnPattern(pattern))
-    return mirror
-})
-console.log(mirrors)
-console.log(mirrors.reduce((sum, mirror) => sum+mirror, 0))
+    const mirrors = patterns.slice(0).map(pattern => {
+        let mirror = findHorizontalMirror(pattern, smudges) * 100
+        if (mirror === 0) mirror = findHorizontalMirror(turnPattern(pattern), smudges)
+        return mirror
+    })
+
+    console.log(mirrors.reduce((sum, mirror) => sum+mirror, 0))
+}
